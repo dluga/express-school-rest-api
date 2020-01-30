078c19edbfd1537436056157ea69380ee38cbfee
dluga <dlugavv@gmail.com>
2020-01-30 15:04:34 +0200

    Use Teacher's router in app. Add home route handler

--- src/app.js
+++ src/app.js
@@ -3,27 +3,28 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db/database');
 const log = require('./helpers/logger')(module);
+const TeacherRouter = require('./routes/Teacher');
 
 const app = express();
 
 // parse application/json
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Express school rest api');
+});
+
+app.use('/api', TeacherRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.listen(process.env.SERVER_PORT, () => {
-  log.info(`App listening on port ${process.env.SERVER_PORT}`);
-});
-
-// error handler
-/* app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -31,4 +32,8 @@ app.listen(process.env.SERVER_PORT, () => {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-}); */
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  log.info(`App listening on port ${process.env.SERVER_PORT}`);
+});
