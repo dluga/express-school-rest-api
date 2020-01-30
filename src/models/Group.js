68ffdf176ff39b8115856a814867ef03f7f3a62e
dluga <dlugavv@gmail.com>
2020-01-30 20:18:18 +0200

    Fix typo in sql queries for "groups" tabel

--- src/models/Group.js
+++ src/models/Group.js
@@ -2,35 +2,35 @@
 const db = require('../db/database');
 
 module.exports.all = (cb) => {
-  const sql = 'SELECT * FROM gropus';
+  const sql = 'SELECT * FROM groups';
   db.all(sql, (err, rows) => {
     cb(err, rows);
   });
 };
 
 module.exports.findById = (params, cb) => {
-  const sql = 'SELECT * FROM gropus WHERE id = ?';
+  const sql = 'SELECT * FROM groups WHERE id = ?';
   db.all(sql, params, (err, row) => {
     cb(err, row);
   });
 };
 
 module.exports.create = (params, cb) => {
-  const sql = 'INSERT INTO gropus (gropu_code) VALUES (?)';
+  const sql = 'INSERT INTO groups (group_code) VALUES (?)';
   db.run(sql, params, function (err) {
     cb(err, { id: this.lastID });
   });
 };
 
 module.exports.update = (params, cb) => {
-  const sql = 'UPDATE gropus SET gropu_code = ? WHERE id = ?';
+  const sql = 'UPDATE groups SET group_code = ? WHERE id = ?';
   db.run(sql, params, (err) => {
     cb(err);
   });
 };
 
 module.exports.delete = (id, cb) => {
-  const sql = 'DELETE FROM gropus WHERE id = ?';
+  const sql = 'DELETE FROM groups WHERE id = ?';
   db.run(sql, id, (err) => {
     cb(err);
   });
