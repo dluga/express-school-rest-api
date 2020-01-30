5022f85b4c500d981575124f6212c11404e1db40
dluga <dlugavv@gmail.com>
2020-01-30 14:59:39 +0200

    Refactoring a file for working with a database

--- src/db/database.js
+++ src/db/database.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const log = require('../helpers/logger')(module);
@@ -11,18 +10,4 @@ const db = new sqlite3.Database(path.join(__dirname, process.env.DBSOURCE), (err
   log.info(`Connected to the ${process.env.DBSOURCE} database.`);
 });
 
-/* module.exports.connect = (done) => {
-  if (db) {
-    return done();
-  }
-
-  db = new sqlite3.Database(path.join(__dirname, process.env.DBSOURCE), (err) => {
-    if (err) {
-      return done(err);
-    }
-    done();
-  });
-};
-
-module.exports.get = () => db; */
 module.exports = db;
